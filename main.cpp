778279783c12a082c18b39f5a53cb30ee7847c9a
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 20:57:08 -0800

    qwae

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 int main()
 {
 	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+for (size_t i = 0; i < 1;)
+{
+	cin>>current;
+	if (current>prev && prev!=0)
+	{
+		cout<<endl;
+		i++;
+		break;
+	}
+	prev=current;
+	cout<<prev<<",";
+}
+
 }
